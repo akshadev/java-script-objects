1a1153ab7034312fefcecd01b037734d87549a16
akshadev <63567120+akshadev@users.noreply.github.com>
2020-05-25 19:07:06 -0400

    Update js-objects.js
    how to sum of all salaries

--- js-objects.js
+++ js-objects.js
@@ -9,3 +9,13 @@ user.name = "pete";
 // remove the property name from object
 delete user.name;
 // the above line deletes user.name
+
+
+// sum all salaries
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+};
+
+alert(salaries.John + salaries.Ann + salaries.Pete);
