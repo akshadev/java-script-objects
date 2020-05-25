e025f175a2fcea58d9d3d5b66ddf9a82ead116a6
akshadev <63567120+akshadev@users.noreply.github.com>
2020-05-25 18:16:08 -0400

    Create js-objects.js
    Explaining object functionality to students

---
+++ js-objects.js
@@ -0,0 +1,10 @@
+// Create an empty object user
+let user = {};
+// add property name with name = john
+let user = { name: "John" };
+// add property name with surname = smith
+user.surname = "Smith";
+// change the value of name to pete
+user.name = "pete";
+// remove the property name from object
+delete user.name;
